1a2a14cb7eba2d30c0213d7b108f80c9d5d7db04
AroLeaf <starleafofa@gmail.com>
2024-02-15 13:55:59 +0100

    [feat] implement adding ships to Fleet

--- src/fleet/fleet.ts
+++ src/fleet/fleet.ts
@@ -1,4 +1,4 @@
-import { Client, Ship } from '..';
+import { ApiTypes, Client, Ship } from '..';
 
 export class Fleet {
   client: Client;
@@ -7,6 +7,14 @@ export class Fleet {
   constructor(client: Client, ships?: Ship[]) {
     this.client = client;
     this.ships  = new Map(ships?.map(ship => [ship.symbol, ship]));
+  }
+
+  addShip(data: ApiTypes.Ship) {
+    const exists = this.ships.get(data.symbol);
+    const ship = exists || new Ship(this.client, data);
+    if (exists) ship.patch(data);
+    else this.ships.set(ship.symbol, ship);
+    return ship;
   }
 }
 
